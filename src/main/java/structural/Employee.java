beb27b38bf638bae5ff217c6863f72e6fc31f4db
Tianhai Liu <tianhai.liu@gmail.com>
2016-02-27 16:36:28 +0100

    add composite pattern

---
+++ src/main/java/structural/Employee.java
@@ -0,0 +1,38 @@
+package structural;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tianhai on 27/02/16 for the project DesignPatterns.
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    // constructor
+    public Employee(String name, String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public String toString() {
+        return ("Employee :[ Name : " + name + ", dept : " + dept + ", salary :" + salary + " ]");
+    }
+}
